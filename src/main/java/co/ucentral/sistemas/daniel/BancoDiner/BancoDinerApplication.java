ac1da717129063c36f08b553fbb184840da237bc
Daniel <danielandrader23@gmail.com>
2024-06-05 21:31:44 -0500

    Master

---
+++ src/main/java/co/ucentral/sistemas/daniel/BancoDiner/BancoDinerApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistemas.daniel.BancoDiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/sistemas/daniel/BancoDiner/BancoDinerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistemas.daniel.BancoDiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
